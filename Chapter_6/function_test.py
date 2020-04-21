12320cccdd77b0acf62da09f2e0fc6ae53952ff3
Joshua McCauley <JDMcCauley@lbl.gov>
2020-04-20 17:03:04 -0700

    Finished Chapter 6 and started Chapter 7.

---
+++ Chapter_6/function_test.py
@@ -0,0 +1,6 @@
+import module_test
+module_test.test("test")
+
+print("Now to pass a number to the function and declare it a variable.")
+result = module_test.test(3)
+print("This is the result of passing 3: ", result)
---
+++ Chapter_6/module_test.py
@@ -0,0 +1,10 @@
+def test(value):
+    """this is a test"""
+    print("This is a print statement.")
+    print("Now I will take the value, and print it.")
+    print(value)
+    print("Now I will multiply the value three times.")
+    three_times = value * 3
+    print(three_times)
+    print("Now to return 3x")
+    return three_times
